75270c71cb585e929c351a36bff01f9a0eac6cc7
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-06-15 13:01:00 -0300

    criação do home-component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `<h1>Angular + Keycloak</h1>`,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
